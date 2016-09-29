8beeab2617ebdb7a8ee0dd5ceef249072341a2fb
Tatchagon <o_k_t@hotmail.com>
2016-09-29 12:30:15 +0700

    Start

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def index
+  	# convert UTF 8 URL to ASCII URL
+  	url = URI.escape params[:url]
+  	@page = Nokogiri::HTML(open(url))
+  	@product_images = @page.css(".detail-img img").to_a
+  	# @root = "root_path(url: params[:url])"
+  	@root = "/?url=#{params[:url]}"
+  end
+end
