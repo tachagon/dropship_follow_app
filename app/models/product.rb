4cbcc5cca14b62100d00aa39c80df99b52d2ac30
Tatchagon <o_k_t@hotmail.com>
2016-10-01 20:59:06 +0700

    temp

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,18 @@ class Product < ApplicationRecord
 	belongs_to :superproduct, class_name: "Product", optional: true
 	has_many :subproducts, class_name: "Product", foreign_key: "superproduct_id", dependent: :destroy
 
+	def show_status
+	  if self.status == "in_stock"
+	  	return "<span style='color: green;'>In Stock</span>"
+		elsif self.status == "out_stock"
+			return "<span style='color: red;'>Out Stock</span>"
+		elsif self.status == "superproduct"
+			return "<span style='color: orange;'>Super Product</span>"
+	  else
+	  	return "<span style='color: gray;'>Not Found</span>"
+	  end
+	end
+
 	scope :single_and_superproduct, -> { where(superproduct: nil)   }
 
 end
